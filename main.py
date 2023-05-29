d9903c290dacc8fb5e1ec0f52f5baa438530c017
Oisin <oisin.leonard@gmail.com>
2023-05-29 09:10:24 +0100

    Added shared entities, remodelled device hash to device type relationship

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # TODO:
-# 2. Add shared entities i.e. shared device hashes, ip hashes and card hashes
 # 3. Add null values
 # 4. change to device and application dictionaries to python classes
+# 5. rewrite using polars / dask
 
+import random
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,15 +12,18 @@ import utilities as utl
 import cons
 from datetime import datetime
 
-# generate random devices
-device_props_dict = utl.cnt2prop_dict(utl.gen_device_type(n_device_types = 20))
-
-application_hashes_dict = utl.gen_application_hash(n_application_hashes = 1000)
-application_hashes_props_dict = utl.cnt2prop_dict(application_hashes_dict)
-application_hash_prices_dict = utl.gen_application_prices(application_hashes_dict)
-
 # set number of users to random generate
 n = 1000
+
+# generate random devices
+device_hashes_dict = utl.gen_hash_dict(n_hashes = n)
+device_props_dict = utl.cnt2prop_dict(device_hashes_dict)
+device_type_dict = utl.gen_device_type(device_hashes_dict, n_device_types = 20)
+
+# generate random applications
+application_hashes_dict = utl.gen_hash_dict(n_hashes = n)
+application_hashes_props_dict = utl.cnt2prop_dict(application_hashes_dict)
+application_hash_prices_dict = utl.gen_application_prices(application_hashes_dict)
 
 # generate counts per entity at user level
 user_data = pd.DataFrame()
@@ -32,19 +36,17 @@ user_data['n_countries'] = np.random.poisson(lam = cons.user_config['lambda']['c
 user_data['n_transactions'] = np.random.poisson(lam = cons.user_config['lambda']['transaction'], size = n) + 1
 
 # generate entities from 
-user_data['device_hash'] = user_data['n_devices'].apply(lambda x: utl.gen_random_hash(size = x, nbytes = 16))
-user_data['device_type'] = user_data['n_devices'].apply(lambda x: np.random.choice(a = list(device_props_dict.keys()), p = list(device_props_dict.values()), replace = True, size = x))
+user_data['device_hash'] = user_data['n_devices'].apply(lambda x: np.random.choice(a = list(device_props_dict.keys()), p = list(device_props_dict.values()), replace = True, size = x))
 user_data['card_hash'] = user_data['n_cards'].apply(lambda x: utl.gen_random_hash(size = x, nbytes = 16))
 user_data['ip_hash'] = user_data['n_ips'].apply(lambda x: utl.gen_random_hash(size = x, nbytes = 16))
 user_data['trans_hash'] = user_data['n_transactions'].apply(lambda x: utl.gen_random_hash(size = x, nbytes = 16))
-user_data['device_type'] = user_data['n_devices'].apply(lambda x: np.random.choice(a = list(device_props_dict.keys()), p = list(device_props_dict.values()), replace = True, size = x))
 user_data['application_hash'] = user_data['n_applications'].apply(lambda x: np.random.choice(a = list(application_hashes_props_dict.keys()), p = list(application_hashes_props_dict.values()), replace = True, size = x))
 
 # explode out transactions
 drop_columns = ['n_devices', 'n_cards', 'n_ips', 'n_applications', 'n_countries', 'n_transactions']
 trans_data = user_data.explode('trans_hash').drop(columns = drop_columns)
 trans_data['device_hash'] = trans_data['device_hash'].apply(lambda x: np.random.choice(x, size = 1)[0])
-trans_data['device_type'] = trans_data['device_type'].apply(lambda x: np.random.choice(x, size = 1)[0])
+trans_data['device_type'] = trans_data['device_hash'].replace(device_type_dict)
 trans_data['card_hash'] = trans_data['card_hash'].apply(lambda x: np.random.choice(x, size = 1)[0])
 trans_data['ip_hash'] = trans_data['ip_hash'].apply(lambda x: np.random.choice(x, size = 1)[0])
 trans_data['application_hash'] = trans_data['application_hash'].apply(lambda x: np.random.choice(x, size = 1)[0])
@@ -60,8 +62,13 @@ trans_data['payment_channel'] = trans_data.apply(lambda s: np.random.choice(a = 
 dates = pd.date_range(datetime.strptime('2021-01-01', '%Y-%m-%d'), datetime.strptime('2021-12-31', '%Y-%m-%d') - pd.Timedelta(days=1),freq='d')
 trans_data['transaction_date'] = trans_data.apply(lambda s: np.random.choice(a = dates, replace = True, size = 1)[0], axis = 1)
 
-# set up
-trans_data['transaction_date']
-
+# add shared entities
+shared_ip = np.random.choice(a = trans_data['ip_hash'].unique(), size = int(np.round(trans_data['ip_hash'].nunique() * cons.shared_entities_dict['ip'])))
+shared_card = np.random.choice(a = trans_data['card_hash'].unique(), size = int(np.round(trans_data['card_hash'].nunique() * cons.shared_entities_dict['card'])))
+shared_device = np.random.choice(a = trans_data['device_hash'].unique(), size = int(np.round(trans_data['device_hash'].nunique() * cons.shared_entities_dict['device'])))
+# add shared entities
+trans_data['ip_hash'] = trans_data['ip_hash'].apply(lambda x: random.choice(shared_ip) if random.uniform(0, 1) <= cons.shared_entities_dict['ip'] else x)
+trans_data['card_hash'] = trans_data['card_hash'].apply(lambda x: random.choice(shared_ip) if random.uniform(0, 1) <= cons.shared_entities_dict['card'] else x)
+trans_data['device_hash'] = trans_data['device_hash'].apply(lambda x: random.choice(shared_ip) if random.uniform(0, 1) <= cons.shared_entities_dict['device'] else x)
 
 trans_data.head()
--- utilities.py
+++ utilities.py
@@ -10,25 +10,28 @@ def cnt2prop_dict(cnt_dict):
         prop_dict[key] = cnt / cnt_total
     return prop_dict
 
-def gen_device_type(n_device_types = 20):
+def gen_device_type(device_hashes_dict, n_device_types = 20):
     """"""
-    device_types_dict = {}
+    device_types_list = []
     letters = list(set(string.ascii_letters.upper()))
     digits = list(string.digits)
     for i in range(n_device_types):
         prefix = ''.join(np.random.choice(letters, size = 3, replace = False))
         suffix = ''.join(np.random.choice(digits, size = 3, replace = False))
         device_type = f'{prefix}-{suffix}'
-        device_types_dict[device_type] = np.random.poisson(lam = 20, size = 1)[0]
+        device_types_list.append(device_type)
+    device_types_dict = {}
+    for key, val in device_hashes_dict.items():
+        device_types_dict[key] = np.random.choice(a = device_types_list, size = 1)[0]
     return device_types_dict
 
-def gen_application_hash(n_application_hashes = 1000):
+def gen_hash_dict(n_hashes = 1000):
     """"""
-    app_hashes_dict = {}
-    for i in range(n_application_hashes):
-        app_hash = gen_random_hash(size = 1, nbytes = 16)[0]
-        app_hashes_dict[app_hash] = np.random.poisson(lam = 20, size = 1)[0]
-    return app_hashes_dict
+    hashes_dict = {}
+    for i in range(n_hashes):
+        hash = gen_random_hash(size = 1, nbytes = 16)[0]
+        hashes_dict[hash] = np.random.poisson(lam = 20, size = 1)[0]
+    return hashes_dict
 
 def gen_application_prices(app_hashes_dict):
     """"""
