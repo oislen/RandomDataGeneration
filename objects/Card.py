ea5037d6376625410a24e9311549e55c73f1efcd
Oisin <oisin.leonard@gmail.com>
2023-05-30 20:13:42 +0100

    Added shared entities

--- objects/Card.py
+++ objects/Card.py
@@ -3,6 +3,7 @@ import cons
 from utilities.gen_idhash_cnt_dict import gen_idhash_cnt_dict
 from utilities.cnt2prop_dict import cnt2prop_dict
 from utilities.gen_country_codes_dict import gen_country_codes_dict
+from utilities.gen_shared_entities import gen_shared_entities
 
 class Card():
 
@@ -10,10 +11,12 @@ class Card():
         self.n_card_hashes = n_card_hashes
         self.card_types = cons.card_types
         self.lam = cons.user_config['lambda']['card']
+        self.prop_shared_card_hashes = cons.shared_entities_dict['card']
         self.card_hashes_cnts_dict = gen_idhash_cnt_dict(idhash_type = 'hash', n = self.n_card_hashes, lam = self.lam)
         self.card_hashes_props_dict = cnt2prop_dict(self.card_hashes_cnts_dict)
         self.card_hashes_type_dict = self.gen_card_type(self.card_hashes_cnts_dict, self.card_types)
         self.card_hashes_country_code_dict = gen_country_codes_dict(self.card_hashes_cnts_dict)
+        self.card_hashes_shared_props_dict = gen_shared_entities(self.card_hashes_cnts_dict, self.prop_shared_card_hashes)
     
     def gen_card_type(self, card_hashes_cnts_dict, card_types):
         """"""
