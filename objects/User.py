106459f521677f2ff8e2b22532f27778824d2281
Oisin <oisin.leonard@gmail.com>
2023-05-30 13:03:46 +0100

    Added user class object and renamed generate country codes dictionary

--- objects/User.py
+++ objects/User.py
@@ -3,20 +3,21 @@ import numpy as np
 import pandas as pd
 from utilities.gen_idhash_cnt_dict import gen_idhash_cnt_dict
 from utilities.cnt2prop_dict import cnt2prop_dict
-from utilities.gen_country_codes import gen_country_codes
+from utilities.gen_country_codes_dict import gen_country_codes_dict
 from utilities.gen_dates_dict import gen_dates_dict
 
 class User():
 
     def __init__(self, n_user_ids, start_date, end_date):
         self.n_user_ids = n_user_ids
+        self.lam = cons.user_config['lambda']['transaction']
         self.start_date = start_date
         self.end_date = end_date
-        self.user_ids_cnts_dict = gen_idhash_cnt_dict(idhash_type = 'id', n = self.n_user_ids)
+        self.user_ids_cnts_dict = gen_idhash_cnt_dict(idhash_type = 'id', n = self.n_user_ids, lam = self.lam)
         self.user_ids_props_dict = cnt2prop_dict(self.user_ids_cnts_dict)
         self.user_ids_firstname_dict = self.gen_user_firstname(cons)
         self.user_ids_lastname_dict = self.gen_user_surname(cons)
-        self.user_ids_country_code_dict = gen_country_codes(self.user_ids_cnts_dict)
+        self.user_ids_country_code_dict = gen_country_codes_dict(self.user_ids_cnts_dict)
         self.user_ids_email_domain_dict = self.gen_user_email_domain(cons)
         self.user_ids_dates_dict = gen_dates_dict(self.user_ids_cnts_dict, start_date = self.start_date, end_date = self.end_date)
     
