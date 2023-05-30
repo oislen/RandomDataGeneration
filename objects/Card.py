ff2b1ec67a9baa21c173302adc11e8ad661367b4
Oisin <oisin.leonard@gmail.com>
2023-05-30 13:06:48 +0100

    Added lambda to card class object and renamed generate country codes dictionary

--- objects/Card.py
+++ objects/Card.py
@@ -2,17 +2,18 @@ import numpy as np
 import cons
 from utilities.gen_idhash_cnt_dict import gen_idhash_cnt_dict
 from utilities.cnt2prop_dict import cnt2prop_dict
-from utilities.gen_country_codes import gen_country_codes
+from utilities.gen_country_codes_dict import gen_country_codes_dict
 
 class Card():
 
     def __init__(self, n_card_hashes):
         self.n_card_hashes = n_card_hashes
         self.card_types = cons.card_types
-        self.card_hashes_cnts_dict = gen_idhash_cnt_dict(idhash_type = 'hash', n = self.n_card_hashes)
+        self.lam = cons.user_config['lambda']['card']
+        self.card_hashes_cnts_dict = gen_idhash_cnt_dict(idhash_type = 'hash', n = self.n_card_hashes, lam = self.lam)
         self.card_hashes_props_dict = cnt2prop_dict(self.card_hashes_cnts_dict)
         self.card_hashes_type_dict = self.gen_card_type(self.card_hashes_cnts_dict, self.card_types)
-        self.card_hashes_country_code_dict = gen_country_codes(self.card_hashes_cnts_dict)
+        self.card_hashes_country_code_dict = gen_country_codes_dict(self.card_hashes_cnts_dict)
     
     def gen_card_type(self, card_hashes_cnts_dict, card_types):
         """"""
