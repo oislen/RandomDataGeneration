2c029596162b5edf7b428bf8878f2ed8716f6287
Oisin <oisin.leonard@gmail.com>
2023-05-30 13:08:10 +0100

    Removed for loop from function and add lambda parameter

--- utilities/gen_idhash_cnt_dict.py
+++ utilities/gen_idhash_cnt_dict.py
@@ -2,13 +2,12 @@ import numpy as np
 from utilities.gen_ranom_hash import gen_random_hash
 from utilities.gen_random_id import gen_random_id
 
-def gen_idhash_cnt_dict(idhash_type, n = 1000):
+def gen_idhash_cnt_dict(idhash_type, n, lam, nbytes = 16):
     """"""
-    idhash_dict = {}
-    for i in range(n):
-        if idhash_type == 'hash':
-            idhash = gen_random_hash(size = 1, nbytes = 16)[0]
-        elif idhash_type == 'id':
-            idhash = gen_random_id(size = 1, nbytes = 16)[0]
-        idhash_dict[idhash] = np.random.poisson(lam = 20, size = 1)[0]
+    if idhash_type == 'hash':
+        idhash_list = gen_random_hash(size = n, nbytes = nbytes)
+    elif idhash_type == 'id':
+        idhash_list = gen_random_id(size = n, nbytes = nbytes)
+    cnts_list = list(np.random.poisson(lam = lam, size = n))
+    idhash_dict = dict(zip(idhash_list, cnts_list))
     return idhash_dict
