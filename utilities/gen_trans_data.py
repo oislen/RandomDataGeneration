376993af9eb18839d2ef26206671bf3e832f4aca
Oisin <oisin.leonard@gmail.com>
2023-05-30 09:07:51 +0100

    Added fix to align registration dates on or before transaction dates

--- utilities/gen_trans_data.py
+++ utilities/gen_trans_data.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 
 def gen_trans_data(user_data, device_obj, card_obj, ip_obj, transaction_obj, application_obj):
     """"""
@@ -16,5 +15,8 @@ def gen_trans_data(user_data, device_obj, card_obj, ip_obj, transaction_obj, app
     trans_data['payment_channel'] = trans_data['transaction_hash'].replace(transaction_obj.transaction_hashes_payment_channel_dict)
     trans_data['transaction_date'] = trans_data['transaction_hash'].replace(transaction_obj.transaction_hashes_dates_dict)
     trans_data['transaction_status'] = trans_data['transaction_hash'].replace(transaction_obj.transaction_hashes_status_dict)
+    # align registration and transaction dates
+    trans_data[['registration_date', 'transaction_date']] = trans_data[['registration_date', 'transaction_date']].apply(lambda s: s.sort_values().to_list(), result_type = 'expand', axis = 1).copy()
+    # sort data by transaction date
     trans_data = trans_data.sort_values(by = 'transaction_date').reset_index(drop = True)
     return trans_data
