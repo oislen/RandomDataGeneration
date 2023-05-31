f6c8772b959b482a2381ba4a3aa7e6ceb80e6bc4
Oisin <oisin.leonard@gmail.com>
2023-05-31 15:37:50 +0100

    Calling random poisson function

--- utilities/gen_idhash_cnt_dict.py
+++ utilities/gen_idhash_cnt_dict.py
@@ -1,6 +1,7 @@
 import numpy as np
 from utilities.gen_ranom_hash import gen_random_hash
 from utilities.gen_random_id import gen_random_id
+from utilities.random_poisson import random_poisson
 
 def gen_idhash_cnt_dict(idhash_type, n, lam, nbytes = 16):
     """"""
@@ -8,6 +9,6 @@ def gen_idhash_cnt_dict(idhash_type, n, lam, nbytes = 16):
         idhash_list = gen_random_hash(size = n, nbytes = nbytes)
     elif idhash_type == 'id':
         idhash_list = gen_random_id(size = n, nbytes = nbytes)
-    cnts_list = list(np.random.poisson(lam = lam, size = n))
+    cnts_list = list(random_poisson(lam = lam, size = n))
     idhash_dict = dict(zip(idhash_list, cnts_list))
     return idhash_dict
