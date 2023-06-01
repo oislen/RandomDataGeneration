355650348d2f177a3dcaccb398591dd3a7ea7c99
Oisin <oisin.leonard@gmail.com>
2023-06-01 12:29:57 +0100

    Reading data from disk instead of across https

--- utilities/gen_country_codes_dict.py
+++ utilities/gen_country_codes_dict.py
@@ -5,7 +5,7 @@ from utilities.cnt2prop_dict import cnt2prop_dict
 
 def gen_country_codes_dict(cnts_dict):
     """"""
-    european_populations_cnt_data = pd.read_csv(cons.european_populations_url, usecols = ['ISO numeric', 'population'])
+    european_populations_cnt_data = pd.read_csv(cons.countrieseurope_fpath, usecols = ['ISO numeric', 'population'])
     european_populations_cnt_dict = european_populations_cnt_data.set_index('ISO numeric').to_dict()['population']
     european_populations_props_dict = cnt2prop_dict(european_populations_cnt_dict)
     idhashes_list = list(cnts_dict.keys())
