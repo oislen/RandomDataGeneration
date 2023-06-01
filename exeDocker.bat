a0ef22ce9f45b3fc7049104bbaada3642781cea2
Oisin <oisin.leonard@gmail.com>
2023-06-01 12:28:46 +0100

    added executable batch script for creating docker container

---
+++ exeDocker.bat
@@ -0,0 +1,17 @@
+:: set docker settings
+SET DOCKER_USER=oislen
+SET DOCKER_REPO=randomtelecomdata
+SET DOCKER_TAG=latest
+SET DOCKER_IMAGE=%DOCKER_USER%/%DOCKER_REPO%:%DOCKER_TAG%
+
+:: remove existing docker containers and images
+docker container prune -f
+docker rm -f %DOCKER_IMAGE%
+
+:: build docker image
+call docker build --no-cache -t %DOCKER_IMAGE% . 
+::call docker build -t %DOCKER_IMAGE% .
+
+:: run docker container
+SET UBUNTU_DIR=/home/ubuntu
+call docker run -it %DOCKER_IMAGE%
