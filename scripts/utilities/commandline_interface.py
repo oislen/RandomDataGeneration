8f514d7a593498de8ea2afabf3e79b3804f3432b
Oisin <oisin.leonard@gmail.com>
2023-06-01 14:11:32 +0100

    Updated help attribute in parser.add_argument

--- scripts/utilities/commandline_interface.py
+++ scripts/utilities/commandline_interface.py
@@ -8,9 +8,9 @@ def commandline_interface():
     # define argument parser object
     parser = argparse.ArgumentParser(description = 'Execute Random TeleCom Data Programme.')
     # add input arguments
-    parser.add_argument("--factor", action = "store", dest = "factor", type = float, default = 1, help = "String, multiplicative effect on number of users to generate")
-    parser.add_argument("--randomseed", action = "store", dest = "randomseed", type = int, default = 0, help = "String, set a random seed for reproducible results; must be either 0 or 1")
-    parser.add_argument("--nitr", action = "store", dest = "nitr", type = int, default = 1, help = "String, number of iterations to run")
+    parser.add_argument("--factor", action = "store", dest = "factor", type = float, default = 1, help = "Float, multiplicative effect on number of users to generate")
+    parser.add_argument("--randomseed", action = "store", dest = "randomseed", type = int, default = 0, help = "Integer, set a random seed for reproducible results; must be either 0 or 1")
+    parser.add_argument("--nitr", action = "store", dest = "nitr", type = int, default = 1, help = "Integer, number of iterations to run")
     # create an output dictionary to hold the results
     input_params_dict = {}
     # extract input arguments 
@@ -19,5 +19,4 @@ def commandline_interface():
     input_params_dict['factor'] = args.factor
     input_params_dict['randomseed'] = args.randomseed
     input_params_dict['nitr'] = args.nitr
-
     return input_params_dict
