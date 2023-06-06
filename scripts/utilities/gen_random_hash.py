8b3ec91153f15c66dcb186d7599b451cbbdbb0bc
Oisin <oisin.leonard@gmail.com>
2023-06-06 11:33:49 +0100

    Renamed and updated random hash function to sample from string letters and asci_lowercase for reproducibility in unittests

---
+++ scripts/utilities/gen_random_hash.py
@@ -0,0 +1,27 @@
+import string
+import numpy as np
+
+
+def gen_random_hash(size, nbytes=16):
+    """Generates a list of random hashes
+
+    Parameters
+    ----------
+    size : int
+        The total number of hashes to generate
+    nbytes : int
+        The number of alphanumeric values in each hash
+
+    Returns
+    -------
+    list
+        A list of random hashes
+    """
+    # generate a list of digits and lower case letters from string library
+    alphanumeric = list(string.digits) + list(string.ascii_lowercase)
+    # randomly sample nbytes digits, string concatenate and convert to integers
+    random_hashes = [
+        "".join(np.random.choice(a=alphanumeric, size=nbytes, replace=True))
+        for i in range(size)
+    ]
+    return random_hashes
