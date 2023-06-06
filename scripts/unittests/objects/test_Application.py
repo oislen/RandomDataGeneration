2fcef26d7794e95ca525044d89a2bfaa20024313
Oisin <oisin.leonard@gmail.com>
2023-06-06 19:10:58 +0100

    #3 Added unittests for Application and Card objects

---
+++ scripts/unittests/objects/test_Application.py
@@ -0,0 +1,73 @@
+import unittest
+import os
+import sys
+import random
+import numpy as np
+
+sys.path.append(os.path.join(os.getcwd(), "scripts"))
+
+from objects.Application import Application
+
+exp_application_hashes_cnts_dict = {'se7kimaanzn2l1nt': 31, '1kwbloqrfe26k8h3': 5, 'od8p1jr67ydgz315': 9, '3shpx9zdue7dmkfh': 6}
+exp_application_hashes_prices_dict = {'se7kimaanzn2l1nt': 1.51, '1kwbloqrfe26k8h3': 0.44, 'od8p1jr67ydgz315': 7.63, '3shpx9zdue7dmkfh': 0.32}
+exp_application_hashes_props_dict = {'se7kimaanzn2l1nt': 0.6078431372549019, '1kwbloqrfe26k8h3': 0.09803921568627451, 'od8p1jr67ydgz315': 0.17647058823529413, '3shpx9zdue7dmkfh': 0.11764705882352941}
+exp_n_application_hashes = 4
+exp_lam = 3
+
+random.seed(42)
+np.random.seed(42)
+application_object = Application(n_application_hashes=exp_n_application_hashes)
+
+obs_application_hashes_cnts_dict = application_object.application_hashes_cnts_dict
+obs_application_hashes_prices_dict = application_object.application_hashes_prices_dict
+obs_application_hashes_props_dict = application_object.application_hashes_props_dict
+obs_lam = application_object.lam
+obs_n_application_hashes = application_object.n_application_hashes
+
+class Test_Application(unittest.TestCase):
+    """"""
+
+    def setUp(self):
+        self.exp_application_hashes_cnts_dict = exp_application_hashes_cnts_dict
+        self.obs_application_hashes_cnts_dict = obs_application_hashes_cnts_dict
+        self.exp_application_hashes_props_dict = exp_application_hashes_props_dict
+        self.obs_application_hashes_props_dict = obs_application_hashes_props_dict
+        self.exp_application_hashes_prices_dict = exp_application_hashes_prices_dict
+        self.obs_application_hashes_prices_dict = obs_application_hashes_prices_dict
+        self.exp_n_application_hashes = exp_n_application_hashes
+        self.obs_n_application_hashes = obs_n_application_hashes
+        self.exp_lam = exp_lam
+        self.obs_lam = obs_lam
+    
+    def test_type(self):
+        self.assertEqual(type(self.obs_application_hashes_cnts_dict), type(self.exp_application_hashes_cnts_dict))
+        self.assertEqual(type(self.obs_application_hashes_props_dict), type(self.exp_application_hashes_props_dict))
+        self.assertEqual(type(self.obs_application_hashes_prices_dict), type(self.exp_application_hashes_prices_dict))
+        self.assertEqual(type(self.obs_n_application_hashes), type(self.exp_n_application_hashes))
+        self.assertEqual(type(self.obs_lam), type(self.exp_lam))
+
+    def test_len(self):
+        self.assertEqual(len(self.obs_application_hashes_props_dict), len(self.exp_application_hashes_props_dict))
+        self.assertEqual(len(self.obs_application_hashes_cnts_dict), len(self.exp_application_hashes_cnts_dict))
+        self.assertEqual(len(self.obs_application_hashes_prices_dict), len(self.exp_application_hashes_prices_dict))
+
+    def test_keys(self):
+        self.assertEqual(list(self.obs_application_hashes_props_dict.keys()), list(self.exp_application_hashes_props_dict.keys()))
+        self.assertEqual(list(self.obs_application_hashes_cnts_dict.keys()), list(self.exp_application_hashes_cnts_dict.keys()))
+        self.assertEqual(list(self.obs_application_hashes_prices_dict.keys()), list(self.exp_application_hashes_prices_dict.keys()))
+
+    def test_values(self):
+        self.assertEqual(list(self.obs_application_hashes_props_dict.values()), list(self.exp_application_hashes_props_dict.values()))
+        self.assertEqual(list(self.obs_application_hashes_cnts_dict.values()), list(self.exp_application_hashes_cnts_dict.values()))
+        self.assertEqual(list(self.obs_application_hashes_prices_dict.values()), list(self.exp_application_hashes_prices_dict.values()))
+
+    def test_object(self):
+        self.assertEqual(self.obs_application_hashes_cnts_dict, self.exp_application_hashes_cnts_dict)
+        self.assertEqual(self.obs_application_hashes_props_dict, self.exp_application_hashes_props_dict)
+        self.assertEqual(self.obs_application_hashes_prices_dict, self.exp_application_hashes_prices_dict)
+        self.assertEqual(self.obs_n_application_hashes, self.exp_n_application_hashes)
+        self.assertEqual(self.obs_lam, self.exp_lam)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ scripts/unittests/objects/test_Card.py
@@ -0,0 +1,106 @@
+import unittest
+import os
+import sys
+import random
+import numpy as np
+
+sys.path.append(os.path.join(os.getcwd(), "scripts"))
+
+from objects.Card import Card
+
+exp_card_hashes_cnts_dict = {'se7kimaanzn2l1nt': 1, '1kwbloqrfe26k8h3': 1, 'od8p1jr67ydgz315': 2, '3shpx9zdue7dmkfh': 1}
+exp_card_hashes_type_dict = {'se7kimaanzn2l1nt': 'visa', '1kwbloqrfe26k8h3': 'visa', 'od8p1jr67ydgz315': 'visa', '3shpx9zdue7dmkfh': 'visa'}
+exp_card_hashes_props_dict = {'se7kimaanzn2l1nt': 0.2, '1kwbloqrfe26k8h3': 0.2, 'od8p1jr67ydgz315': 0.4, '3shpx9zdue7dmkfh': 0.2}
+exp_card_hashes_country_code_dict = {'se7kimaanzn2l1nt': 642, '1kwbloqrfe26k8h3': 620, 'od8p1jr67ydgz315': 826, '3shpx9zdue7dmkfh': 528}
+exp_card_hashes_shared_props_dict = {}
+exp_card_types_dict = {'visa': 0.5, 'mastercard': 0.5}
+exp_prop_shared_card_hashes = 0.005
+exp_n_card_hashes = 4
+exp_lam = 0.1
+
+random.seed(42)
+np.random.seed(42)
+card_object = Card(n_card_hashes=exp_n_card_hashes)
+
+obs_card_hashes_cnts_dict = card_object.card_hashes_cnts_dict
+obs_card_types_dict = card_object.card_types_dict
+obs_card_hashes_type_dict = card_object.card_hashes_type_dict
+obs_card_hashes_props_dict = card_object.card_hashes_props_dict
+obs_card_hashes_country_code_dict = card_object.card_hashes_country_code_dict
+obs_card_hashes_shared_props_dict = card_object.card_hashes_shared_props_dict
+obs_prop_shared_card_hashes = card_object.prop_shared_card_hashes
+obs_lam = card_object.lam
+obs_n_card_hashes = card_object.n_card_hashes
+
+class Test_Application(unittest.TestCase):
+    """"""
+
+    def setUp(self):
+        self.exp_card_hashes_cnts_dict = exp_card_hashes_cnts_dict
+        self.obs_card_hashes_cnts_dict = obs_card_hashes_cnts_dict
+        self.exp_card_types_dict = exp_card_types_dict
+        self.obs_card_types_dict = obs_card_types_dict
+        self.exp_card_hashes_type_dict = exp_card_hashes_type_dict
+        self.obs_card_hashes_type_dict = obs_card_hashes_type_dict
+        self.exp_card_hashes_props_dict = exp_card_hashes_props_dict
+        self.obs_card_hashes_props_dict = obs_card_hashes_props_dict
+        self.exp_card_hashes_country_code_dict = exp_card_hashes_country_code_dict
+        self.obs_card_hashes_country_code_dict = obs_card_hashes_country_code_dict
+        self.exp_card_hashes_shared_props_dict = exp_card_hashes_shared_props_dict
+        self.obs_card_hashes_shared_props_dict = obs_card_hashes_shared_props_dict
+        self.exp_prop_shared_card_hashes = exp_prop_shared_card_hashes
+        self.obs_prop_shared_card_hashes = obs_prop_shared_card_hashes
+        self.exp_lam = exp_lam
+        self.obs_lam = obs_lam
+        self.exp_n_card_hashes = exp_n_card_hashes
+        self.obs_n_card_hashes = obs_n_card_hashes
+    
+    def test_type(self):
+        self.assertEqual(type(self.obs_card_hashes_cnts_dict), type(self.exp_card_hashes_cnts_dict))
+        self.assertEqual(type(self.obs_card_types_dict), type(self.exp_card_types_dict))
+        self.assertEqual(type(self.obs_card_hashes_type_dict), type(self.exp_card_hashes_type_dict))
+        self.assertEqual(type(self.obs_card_hashes_props_dict), type(self.exp_card_hashes_props_dict))
+        self.assertEqual(type(self.obs_prop_shared_card_hashes), type(self.exp_prop_shared_card_hashes))
+        self.assertEqual(type(self.obs_card_hashes_country_code_dict), type(self.exp_card_hashes_country_code_dict))
+        self.assertEqual(type(self.obs_card_hashes_shared_props_dict), type(self.exp_card_hashes_shared_props_dict))
+        self.assertEqual(type(self.obs_lam), type(self.exp_lam))
+        self.assertEqual(type(self.obs_n_card_hashes), type(self.exp_n_card_hashes))
+
+    def test_len(self):
+        self.assertEqual(len(self.obs_card_hashes_cnts_dict), len(self.exp_card_hashes_cnts_dict))
+        self.assertEqual(len(self.obs_card_types_dict), len(self.exp_card_types_dict))
+        self.assertEqual(len(self.obs_card_hashes_type_dict), len(self.exp_card_hashes_type_dict))
+        self.assertEqual(len(self.obs_card_hashes_props_dict), len(self.exp_card_hashes_props_dict))
+        self.assertEqual(len(self.obs_card_hashes_country_code_dict), len(self.exp_card_hashes_country_code_dict))
+        self.assertEqual(len(self.obs_card_hashes_shared_props_dict), len(self.exp_card_hashes_shared_props_dict))
+
+    def test_keys(self):
+        self.assertEqual(list(self.obs_card_hashes_cnts_dict.keys()), list(self.exp_card_hashes_cnts_dict.keys()))
+        self.assertEqual(list(self.obs_card_types_dict.keys()), list(self.exp_card_types_dict.keys()))
+        self.assertEqual(list(self.obs_card_hashes_type_dict.keys()), list(self.exp_card_hashes_type_dict.keys()))
+        self.assertEqual(list(self.obs_card_hashes_props_dict.keys()), list(self.exp_card_hashes_props_dict.keys()))
+        self.assertEqual(list(self.obs_card_hashes_country_code_dict.keys()), list(self.exp_card_hashes_country_code_dict.keys()))
+        self.assertEqual(list(self.obs_card_hashes_shared_props_dict.keys()), list(self.exp_card_hashes_shared_props_dict.keys()))
+
+    def test_values(self):
+        self.assertEqual(list(self.obs_card_hashes_cnts_dict.values()), list(self.exp_card_hashes_cnts_dict.values()))
+        self.assertEqual(list(self.obs_card_types_dict.values()), list(self.exp_card_types_dict.values()))
+        self.assertEqual(list(self.obs_card_hashes_type_dict.values()), list(self.exp_card_hashes_type_dict.values()))
+        self.assertEqual(list(self.obs_card_hashes_props_dict.values()), list(self.exp_card_hashes_props_dict.values()))
+        self.assertEqual(list(self.obs_card_hashes_country_code_dict.values()), list(self.exp_card_hashes_country_code_dict.values()))
+        self.assertEqual(list(self.obs_card_hashes_shared_props_dict.values()), list(self.exp_card_hashes_shared_props_dict.values()))
+
+    def test_object(self):
+        self.assertEqual(self.obs_card_hashes_cnts_dict, self.exp_card_hashes_cnts_dict)
+        self.assertEqual(self.obs_card_types_dict, self.exp_card_types_dict)
+        self.assertEqual(self.obs_card_hashes_type_dict, self.exp_card_hashes_type_dict)
+        self.assertEqual(self.obs_card_hashes_props_dict, self.exp_card_hashes_props_dict)
+        self.assertEqual(self.obs_prop_shared_card_hashes, self.exp_prop_shared_card_hashes)
+        self.assertEqual(self.obs_card_hashes_country_code_dict, self.exp_card_hashes_country_code_dict)
+        self.assertEqual(self.obs_card_hashes_shared_props_dict, self.exp_card_hashes_shared_props_dict)
+        self.assertEqual(self.obs_lam, self.exp_lam)
+        self.assertEqual(self.obs_n_card_hashes, self.exp_n_card_hashes)
+
+
+if __name__ == "__main__":
+    unittest.main()
