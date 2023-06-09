9cb1dba0c7564932e2eac724156ef82986b7be0f
Oisin <oisin.leonard@gmail.com>
2023-06-09 10:38:42 +0100

    Added debug mode as an input programme parameter

--- scripts/app/ProgrammeParams.py
+++ scripts/app/ProgrammeParams.py
@@ -3,12 +3,13 @@ import cons
 
 class ProgrammeParams():
     
-    def __init__(self, factor = 1, randomseed = None, n_users = 1372, n_applications = 16472, n_device_types = 53, registration_start_date = '2020-01-01', registration_end_date = '2021-12-31', transaction_start_date = '2021-01-01', transaction_end_date = '2021-12-31'):
+    def __init__(self, factor = 1, randomseed = None, debug_mode = False, n_users = 1372, n_applications = 16472, n_device_types = 53, registration_start_date = '2020-01-01', registration_end_date = '2021-12-31', transaction_start_date = '2021-01-01', transaction_end_date = '2021-12-31'):
         # if running in debug mode
-        if cons.debug_mode:
+        if debug_mode:
             # take programme parameters from constants file
             self.factor = cons.programme_parameters_factor
             self.randomseed = cons.programme_parameters_randomseed
+            self.debug_mode = cons.debug_mode
             self.n_users = int(np.round(cons.programme_parameters_n_users * cons.programme_parameters_factor))
             self.n_applications = cons.programme_parameters_n_applications
             self.n_device_types = cons.programme_parameters_n_device_types
@@ -20,6 +21,7 @@ class ProgrammeParams():
             # take programme parameters from class parameters
             self.factor = factor
             self.randomseed = randomseed
+            self.debug_mode = debug_mode
             self.n_users = int(np.round(n_users * factor))
             self.n_applications = n_applications
             self.n_device_types = n_device_types
