8d3d9cc80c5bdc363869208079b0c821116448ea
Oisin <oisin.leonard@gmail.com>
2023-06-10 12:49:25 +0100

    Moved gen_trans_rejection_rates and gen_trans_status functions to utilities directory

--- scripts/app/gen_trans_data.py
+++ scripts/app/gen_trans_data.py
@@ -5,81 +5,9 @@ import cons
 from utilities.gen_country_codes_map import gen_country_codes_map
 from utilities.align_country_codes import align_country_codes
 from utilities.gen_trans_error_codes import gen_trans_error_codes
+from utilities.gen_trans_rejection_rates import gen_trans_rejection_rates
+from utilities.gen_trans_status import gen_trans_status
 
-def gen_trans_rejection_rates(trans_data):
-    """"""
-    # create empty dictionary to hold rejection rates
-    rejection_rates_dict = {}
-
-    # generate country code rejection based rates
-    countrieseurope = pd.read_csv(cons.fpath_countrieseurope, usecols = ['ISO numeric', 'ISO alpha 2'])
-    countrycrimeindex = pd.read_csv(cons.fpath_countrycrimeindex, usecols = ['country_code', 'crime_index'])
-    europecountrycrimeindex = pd.merge(left = countrieseurope, right = countrycrimeindex, left_on = 'ISO alpha 2', right_on = 'country_code', how = 'left')
-    europecountrycrimeindex['trans_reject_rate'] = europecountrycrimeindex['crime_index'].divide(europecountrycrimeindex['crime_index'].sum())
-    country_code_trans_reject_rate_dict = europecountrycrimeindex.set_index('ISO alpha 2')['trans_reject_rate'].to_dict()
-    rejection_rates_dict['country_code_trans_reject_rate_dict'] = country_code_trans_reject_rate_dict
-
-    # generate domain email rejection based rates
-    domain_email = pd.read_csv(cons.fpath_domain_email, usecols = ['domain', 'proportion'])
-    domain_email['trans_reject_rate'] = (1 - domain_email['proportion']) / (1 - domain_email['proportion']).sum()
-    domain_email_trans_reject_rate_dict = domain_email.set_index('domain')['trans_reject_rate'].to_dict()
-    rejection_rates_dict['domain_email_trans_reject_rate_dict'] = domain_email_trans_reject_rate_dict
-
-    # generate shared entities with rejection rates dictionary
-    shared_devices = trans_data.groupby(by = 'device_hash').agg({'userid':'nunique'}).sort_values(by = 'userid')
-    shared_ips = trans_data.groupby(by = 'ip_hash').agg({'userid':'nunique'}).sort_values(by = 'userid')
-    shared_cards = trans_data.groupby(by = 'card_hash').agg({'userid':'nunique'}).sort_values(by = 'userid')
-    shared_devices_reject_rate_dict = shared_devices.divide(shared_devices['userid'].sum()).to_dict()['userid']
-    shared_ips_reject_rate_dict = shared_ips.divide(shared_ips['userid'].sum()).to_dict()['userid']
-    shared_cards_reject_rate_dict = shared_cards.divide(shared_cards['userid'].sum()).to_dict()['userid']
-    rejection_rates_dict['shared_devices_reject_rate_dict'] = shared_devices_reject_rate_dict
-    rejection_rates_dict['shared_ips_reject_rate_dict'] = shared_ips_reject_rate_dict
-    rejection_rates_dict['shared_cards_reject_rate_dict'] = shared_cards_reject_rate_dict
-
-    # generate occurence based rejection rates
-    count_devices = trans_data.groupby(by = 'userid').agg({'device_hash':'nunique'}).sort_values(by = 'device_hash')
-    count_ips = trans_data.groupby(by = 'userid').agg({'ip_hash':'nunique'}).sort_values(by = 'ip_hash')
-    count_cards = trans_data.groupby(by = 'userid').agg({'card_hash':'nunique'}).sort_values(by = 'card_hash')
-    count_devices_reject_rate_dict = count_devices.divide(count_devices['device_hash'].sum()).to_dict()['device_hash']
-    count_ips_reject_rate_dict = count_ips.divide(count_ips['ip_hash'].sum()).to_dict()['ip_hash']
-    count_cards_reject_rate_dict = count_cards.divide(count_cards['card_hash'].sum()).to_dict()['card_hash']
-    rejection_rates_dict['count_devices_reject_rate_dict'] = count_devices_reject_rate_dict
-    rejection_rates_dict['count_ips_reject_rate_dict'] = count_ips_reject_rate_dict
-    rejection_rates_dict['count_cards_reject_rate_dict'] = count_cards_reject_rate_dict
-
-    return rejection_rates_dict
-
-def gen_trans_status(series, rejection_rates_dict):
-    """"""
-    # set country code columns
-    country_code_columns = ['registration_country_code', 'ip_country_code', 'card_country_code']
-    # add rejections based on crime rates within country codes
-    if rejection_rates_dict['country_code_trans_reject_rate_dict'][np.random.choice(a = series[country_code_columns].dropna().to_list(), size = 1)[0]] >= random.uniform(0,1):
-        status = 'rejected'
-    # add rejections based on domain frequencies
-    elif rejection_rates_dict['domain_email_trans_reject_rate_dict'][series['email_domain']] >= random.uniform(0,1):
-        status = 'rejected'
-    # add rejections based on inconsistent country codes
-    elif cons.data_model_inconsistent_country_codes_rejection_rate[series[country_code_columns].dropna().nunique()] >= random.uniform(0,1):
-        status = 'rejected'
-    # add rejections based on shared ips, cards and devices
-    elif series['device_hash'] == series['device_hash'] and rejection_rates_dict['shared_devices_reject_rate_dict'][series['device_hash']] >= random.uniform(0,1):
-        status = 'rejected'
-    elif series['ip_hash'] == series['ip_hash'] and rejection_rates_dict['shared_ips_reject_rate_dict'][series['ip_hash']] >= random.uniform(0,1):
-        status = 'rejected'
-    elif series['card_hash'] == series['card_hash'] and rejection_rates_dict['shared_cards_reject_rate_dict'][series['card_hash']] >= random.uniform(0,1):
-        status = 'rejected'
-    # add rejections based on counts of devices, ips and cards
-    elif rejection_rates_dict['count_devices_reject_rate_dict'][series['userid']] >= random.uniform(0,1):
-        status = 'rejected'
-    elif rejection_rates_dict['count_ips_reject_rate_dict'][series['userid']] >= random.uniform(0,1):
-        status = 'rejected'
-    elif rejection_rates_dict['count_cards_reject_rate_dict'][series['userid']] >= random.uniform(0,1):
-        status = 'rejected'
-    # otherwise return successful status
-    else:
-        status = series['transaction_status']
-    return status
 
 def gen_trans_data(user_data, device_obj, card_obj, ip_obj, transaction_obj, application_obj):
     """Generates random transaction level telecom payments data
