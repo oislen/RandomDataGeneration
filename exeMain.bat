cc0955bfdffacffd03229d430c0e369516033917
Oisin <oisin.leonard@gmail.com>
2024-09-17 08:33:32 +0100

    #5 Added test case line for generating data

--- exeMain.bat
+++ exeMain.bat
@@ -1,1 +1,2 @@
-python3 -m pdb scripts/main.py --factor 0.5 --randomseed 1 --nitr 3
+:: python3 -m pdb scripts/main.py --factor 0.5 --randomseed 1 --nitr 3
+python3 scripts/main.py --factor 5 --randomseed 1 --nitr 2
