9788d520b709da2c99474a7feee2666af25bc6f5
Oisin <oisin.leonard@gmail.com>
2024-09-22 18:57:34 +0100

    Increased factor to 6.5 to align output record shape with existing version 1

--- exeMain.bat
+++ exeMain.bat
@@ -1,2 +1,2 @@
 python -m pdb scripts/main.py --factor 0.02 --randomseed 1 --nitr 1
-:: python scripts/main.py --factor 5 --randomseed 1 --nitr 2
+:: python scripts/main.py --factor 6.5 --randomseed 1 --nitr 2
