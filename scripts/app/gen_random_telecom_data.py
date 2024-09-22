e936d64a163347d2270e61348bb648b80e6892d9
Oisin <oisin.leonard@gmail.com>
2024-09-22 11:07:52 +0100

    Updated logic to generate Users object first, then calculate poisson counts for all entities, and then use the poisson counts to generate the exact number of device, card, ip and transaction hashes needed.

--- scripts/app/gen_random_telecom_data.py
+++ scripts/app/gen_random_telecom_data.py
@@ -9,7 +9,7 @@ from objects.Device import Device
 from objects.Ip import Ip
 from objects.Transaction import Transaction
 from objects.User import User
-
+from utilities.gen_random_entity_counts import gen_random_entity_counts
 
 def gen_random_telecom_data(factor=1, randomseed=None, debug_mode=False):
     """Generates random telecommunications data
@@ -36,37 +36,22 @@ def gen_random_telecom_data(factor=1, randomseed=None, debug_mode=False):
     random.seed(programmeparams.randomseed)
     np.random.seed(seed=programmeparams.randomseed)
 
-    # generate random applications
-    application_obj = Application(n_application_hashes=programmeparams.n_applications)
-
-    # generate random cards
-    card_obj = Card(n_card_hashes=programmeparams.n_cards)
-
-    # generate random devices
-    device_obj = Device(
-        n_device_hashes=programmeparams.n_devices,
-        n_device_types=programmeparams.n_device_types,
-    )
-
-    # generate random ips
-    ip_obj = Ip(n_ip_hashes=programmeparams.n_ips)
-
-    # generate random transactions
-    transaction_obj = Transaction(
-        n_transaction_hashes=programmeparams.n_transactions,
-        start_date=programmeparams.transaction_start_date,
-        end_date=programmeparams.transaction_end_date,
-    )
-
     # generate random users
-    user_obj = User(
-        n_user_ids=programmeparams.n_users,
-        start_date=programmeparams.registration_start_date,
-        end_date=programmeparams.registration_end_date,
-    )
+    user_obj = User(n_user_ids=programmeparams.n_users, start_date=programmeparams.registration_start_date, end_date=programmeparams.registration_end_date)
+
+    # generate random entity counts for each user
+    random_entity_counts = gen_random_entity_counts(user_obj)
+
+    # generate random entity values
+    device_obj = Device(n_device_hashes=random_entity_counts['n_devices'].sum(), n_device_types=programmeparams.n_device_types)
+    card_obj = Card(n_card_hashes=random_entity_counts['n_cards'].sum())
+    ip_obj = Ip(n_ip_hashes=random_entity_counts['n_ips'].sum())
+    transaction_obj = Transaction(n_transaction_hashes=random_entity_counts['n_transactions'].sum(), start_date=programmeparams.transaction_start_date, end_date=programmeparams.transaction_end_date)
+    application_obj = Application(n_application_hashes=programmeparams.n_applications)
 
     # generate user level data
     user_data = gen_user_data(
+        random_entity_counts=random_entity_counts,
         user_obj=user_obj,
         device_obj=device_obj,
         card_obj=card_obj,
