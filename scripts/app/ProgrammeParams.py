0d1fe9e967d5349637e00896ac6212c5a56890d2
Oisin <oisin.leonard@gmail.com>
2024-09-22 11:01:14 +0100

    Removed calculations of n entity counts and ratios. Defaulting to 2000 users, 20000 applications and 200 devices.

--- scripts/app/ProgrammeParams.py
+++ scripts/app/ProgrammeParams.py
@@ -3,7 +3,7 @@ import cons
 
 class ProgrammeParams():
     
-    def __init__(self, factor = 1, randomseed = None, debug_mode = False, n_users = 1372, n_applications = 16472, n_device_types = 53, registration_start_date = '2020-01-01', registration_end_date = '2020-12-31', transaction_start_date = '2021-01-01', transaction_end_date = '2021-12-31'):
+    def __init__(self, factor = 1, randomseed = None, debug_mode = False, n_users = 2000, n_applications = 20000, n_device_types = 200, registration_start_date = '2020-01-01', registration_end_date = '2020-12-31', transaction_start_date = '2021-01-01', transaction_end_date = '2021-12-31'):
         # if running in debug mode
         if debug_mode:
             # take programme parameters from constants file
@@ -29,13 +29,3 @@ class ProgrammeParams():
             self.registration_end_date = registration_end_date
             self.transaction_start_date = transaction_start_date
             self.transaction_end_date = transaction_end_date
-        # extract out the entity user ratios
-        self.card_user_ratio = cons.data_model_entity_user_ratios['card']
-        self.device_user_ratio = cons.data_model_entity_user_ratios['device']
-        self.transaction_user_ratio = cons.data_model_entity_user_ratios['transaction']
-        self.ip_user_ratio = cons.data_model_entity_user_ratios['ip']
-        # determine the number of each entity
-        self.n_cards = int(np.round(self.n_users * self.card_user_ratio))
-        self.n_devices = int(np.round(self.n_users * self.device_user_ratio))
-        self.n_transactions = int(np.round(self.n_users * self.transaction_user_ratio))
-        self.n_ips = int(np.round(self.n_users * self.ip_user_ratio))
