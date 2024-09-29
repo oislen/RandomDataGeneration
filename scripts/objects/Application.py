7b27d51af91b2962adb9080cab9af7c56d97f660
Oisin <oisin.leonard@gmail.com>
2024-09-29 13:14:28 +0100

    Added gen_transaction_payment_channel to application sobject

--- scripts/objects/Application.py
+++ scripts/objects/Application.py
@@ -28,6 +28,35 @@ class Application:
         self.n_application_hashes = n_application_hashes
         self.lam = cons.data_model_poisson_params["application"]["lambda"]
         self.power = cons.data_model_poisson_params["application"]["power"]
+        self.payment_channels = cons.data_model_payment_channels
         self.application_hashes_cnts_dict = gen_idhash_cnt_dict(idhash_type="hash", n=self.n_application_hashes, lam=self.lam)
         self.application_hashes_props_dict = cnt2prop_dict(self.application_hashes_cnts_dict)
+        self.application_hashes_payment_channel_dict = self.gen_transaction_payment_channel(list(self.application_hashes_cnts_dict.keys()), self.payment_channels)
 
+    def gen_transaction_payment_channel(self, application_hashes, payment_channels):
+        """Generates a dictionary of random application payment channels
+
+        Parameters
+        ----------
+        application_hashes : list
+            The application hashes
+        payment_channels : dict
+            The population proportion of payment channels
+
+        Returns
+        -------
+        dict
+            A dictionary of transaction payment channels
+        """
+        # randomly sample payment channels based on population proportions
+        transactoin_payment_channels = list(
+            np.random.choice(
+                a=list(payment_channels.keys()),
+                p=list(payment_channels.values()),
+                replace=True,
+                size=len(application_hashes),
+            )
+        )
+        # return payment channels and application hashes
+        application_hashes_payment_channels_dict = dict(zip(application_hashes, transactoin_payment_channels))
+        return application_hashes_payment_channels_dict
