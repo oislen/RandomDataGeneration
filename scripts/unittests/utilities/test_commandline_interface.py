d2a2ec3756da4694298a68bd604271afd8414cdc
Oisin <oisin.leonard@gmail.com>
2024-10-03 20:47:13 +0100

    Created unittests for round trans amount and commandline interface

---
+++ scripts/unittests/utilities/test_commandline_interface.py
@@ -0,0 +1,49 @@
+import unittest
+import os
+import sys
+import numpy as np
+import pandas as pd
+import random
+
+sys.path.append(os.path.join(os.getcwd(), "scripts"))
+
+import cons
+from utilities.commandline_interface import commandline_interface
+
+random.seed(cons.unittest_seed)
+
+if False and __name__ == "__main__":
+
+    obs_input_params_dict = commandline_interface()
+    obs_n_users = obs_input_params_dict["n_users"]
+    obs_use_random_seed = obs_input_params_dict["use_random_seed"]
+    obs_n_itr = obs_input_params_dict["n_itr"]
+
+    exp_n_users = 100
+    exp_use_random_seed = 0
+    exp_n_itr = 1
+
+
+    class Test_commandline_interface(unittest.TestCase):
+        """"""
+
+        def setUp(self):
+            self.obs_n_users = obs_n_users
+            self.exp_n_users = exp_n_users
+            self.obs_use_random_seed = obs_use_random_seed
+            self.exp_use_random_seed = exp_use_random_seed
+            self.obs_n_itr = obs_n_itr
+            self.exp_n_itr = exp_n_itr
+
+        def test_type(self):
+            self.assertEqual(type(self.obs_n_users),type(self.exp_n_users),)
+            self.assertEqual(type(self.obs_use_random_seed),type(self.exp_use_random_seed),)
+            self.assertEqual(type(self.obs_n_itr), type(self.exp_n_itr))
+
+        def test_object(self):
+            self.assertEqual(self.obs_n_users, self.exp_n_users)
+            self.assertEqual(self.obs_use_random_seed,self.exp_use_random_seed,)
+            self.assertEqual(self.obs_n_itr, self.exp_n_itr)
+
+
+    unittest.main()
---
+++ scripts/unittests/utilities/test_round_trans_amount.py
@@ -0,0 +1,39 @@
+import unittest
+import os
+import sys
+import numpy as np
+import pandas as pd
+import random
+
+sys.path.append(os.path.join(os.getcwd(), "scripts"))
+
+import cons
+from utilities.round_trans_amount import round_trans_amount
+
+random.seed(cons.unittest_seed)
+np.random.seed(seed=cons.unittest_seed)
+
+obs_amount = round_trans_amount(amounts=np.array([1, 2, 3]))
+exp_amount = np.array([0.99, 1, 2.59])
+
+
+class Test_round_trans_amount(unittest.TestCase):
+    """"""
+
+    def setUp(self):
+        self.obs_amount = obs_amount
+        self.exp_amount = exp_amount
+
+    def test_type(self):
+        self.assertEqual(type(self.obs_amount),type(self.exp_amount),)
+
+    def test_shape(self):
+        self.assertEqual(type(self.obs_amount.shape),type(self.exp_amount.shape),)
+
+    def test_object(self):
+        self.assertTrue((self.obs_amount == self.exp_amount).all())
+
+
+if __name__ == "__main__":
+
+    unittest.main()
