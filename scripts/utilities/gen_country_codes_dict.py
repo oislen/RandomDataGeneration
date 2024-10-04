d34df53ce2834d83cc725f7055a540abffecf14c
Oisin <oisin.leonard@gmail.com>
2024-10-04 13:18:14 +0100

    Parameterised fpath_countrieseurope, and set default cons value

--- scripts/utilities/gen_country_codes_dict.py
+++ scripts/utilities/gen_country_codes_dict.py
@@ -4,7 +4,7 @@ import pandas as pd
 from utilities.cnt2prop_dict import cnt2prop_dict
 
 
-def gen_country_codes_dict(idhashes_cnts_dict):
+def gen_country_codes_dict(idhashes_cnts_dict, fpath_countrieseurope = cons.fpath_countrieseurope):
     """Generates a dictionary of random country codes for an input dictionary of idhashes counts
 
     Parameters
@@ -19,7 +19,7 @@ def gen_country_codes_dict(idhashes_cnts_dict):
     """
 
     # load population data of european countries
-    european_populations_cnt_data = pd.read_csv(filepath_or_buffer=cons.fpath_countrieseurope, usecols=["ISO numeric", "population"],)
+    european_populations_cnt_data = pd.read_csv(filepath_or_buffer=fpath_countrieseurope, usecols=["ISO numeric", "population"],)
     # convert to a dictionary of ISO country codes with population counts
     european_populations_cnt_dict = european_populations_cnt_data.set_index("ISO numeric").to_dict()["population"]
     # convert dictionary of population counts to dictionary of population proportions
