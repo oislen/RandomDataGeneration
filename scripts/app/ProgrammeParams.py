5f250f361a4ba7b0c87df158c8b6bb4cb422f5c8
Oisin <oisin.leonard@gmail.com>
2024-10-06 15:57:29 +0100

    #14 added logic to calculate transaction timescale

--- scripts/app/ProgrammeParams.py
+++ scripts/app/ProgrammeParams.py
@@ -1,9 +1,9 @@
-import numpy as np
 import cons
+from datetime import datetime
 
 class ProgrammeParams():
     
-    def __init__(self, n_users = 100, random_seed = None, n_applications = 20000, registration_start_date = '2020-01-01', registration_end_date = '2020-12-31', transaction_start_date = '2021-01-01', transaction_end_date = '2021-12-31'):
+    def __init__(self, n_users=100, random_seed=None, n_applications=20000, registration_start_date='2020-01-01', registration_end_date='2020-12-31', transaction_start_date='2021-01-01', transaction_end_date='2021-12-31'):
         # take programme parameters from class parameters
         self.random_seed = random_seed
         self.n_users = n_users
@@ -12,3 +12,6 @@ class ProgrammeParams():
         self.registration_end_date = registration_end_date
         self.transaction_start_date = transaction_start_date
         self.transaction_end_date = transaction_end_date
+        transaction_start_date_strftime = datetime.strptime(self.transaction_start_date, cons.date_date_strftime)
+        transaction_end_date_strftime = datetime.strptime(self.transaction_end_date, cons.date_date_strftime)
+        self.transaction_timescale = ((transaction_end_date_strftime - transaction_start_date_strftime).days + 1) / 365
