4a8c0900143fc2581e35d6b840bb75dfcf264160
Oisin <oisin.leonard@gmail.com>
2024-10-07 22:20:26 +0100

    #21 Updated script to use local instance of llama 2 chat

--- scripts/utilities/gen_user_names_file.py
+++ scripts/utilities/gen_user_names_file.py
@@ -1,22 +1,48 @@
-import transformers
-import torch
+import json
+import os
+import sys
 
-model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+# set huggingface hub directory
+huggingface_hub_dir = 'E:\\huggingface\\hub'
+os.environ['HF_HOME'] = huggingface_hub_dir
+os.environ['HF_DATASETS_CACHE'] = huggingface_hub_dir
+os.environ['TORCH_HOME'] = huggingface_hub_dir
+
+
+import torch
+torch.cuda.is_available()
+# Output should be True
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import transformers
+from huggingface_hub import login
+
+
+# load huggingface access token
+with open('E:\\GitHub\\RandomTelecomPayments\\.creds\\huggingface.json') as f:
+    token = json.load(f)
+# log into huggingface
+login(token = token)
+
+access_token = "Enter your token here"
+model = "meta-llama/Llama-2-7b-chat-hf"
+
+tokenizer = AutoTokenizer.from_pretrained(model, token=access_token)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model, 
+    token=access_token
+)
 
 pipeline = transformers.pipeline(
     "text-generation",
-    model=model_id,
-    model_kwargs={"torch_dtype": torch.bfloat16},
+    model=model,
+    torch_dtype=torch.float16,
     device_map="auto",
 )
 
-messages = [
-    {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
-    {"role": "user", "content": "Who are you?"},
-]
-
-outputs = pipeline(
-    messages,
-    max_new_tokens=256,
+sequences = pipeline(
+    'Hi! Tell me about yourself!',
+    do_sample=True,
 )
-print(outputs[0]["generated_text"][-1])
+print(sequences[0].get("generated_text"))
