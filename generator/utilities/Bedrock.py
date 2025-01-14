08eda49bec2ee714979693b0db5a2960ad2a97f8
Oisin <oisin.leonard@gmail.com>
2025-01-14 10:58:06 +0000

    Revised prompt. Added examples. Restructured output json structure into a lists

--- generator/utilities/Bedrock.py
+++ generator/utilities/Bedrock.py
@@ -46,23 +46,21 @@ system = """
 # Task
 
 You are a random name generator for users from different countries in Europe.
-Your task is to generate an arbitrary number of typical / popular firstname and surname pairs for both male and female users from given a country of origin.
-Do not repeat any firstnames or surnames, as each individual firstname must be unique and each individual surname must be unique.
+Your task is to generate an arbitrary N number of typical / popular firstnames and lastnames for both male and female users from given a country of origin.
+Do not repeat any firstnames or lastnames, as each individual firstname must be unique and each individual lastname must be unique.
 You should return the random user names using a valid JSON record set tagged as <answer></answer>.
 The valid JSON record set should be of the following structure
 
-[{"firstname":"user_firstname_1", "lastname":"user_lastname_1"},{"firstname":"user_firstname_2", "lastname":"user_lastname_2"},...,{"firstname":"user_firstname_n", "lastname":"user_lastname_n"}]
+{"firstnames":["user_firstname_1","user_firstname_2",...,"user_firstname_N"], "lastnames":["user_lastname_1","user_lastname_2",...,"user_lastname_N"]}
 
-# Example
+# Examples
 
-Generate 4 user names for "United Kingdom"
-
-<answer>[{"firstname":"George","lastname":"Smith"},{"firstname":"Richard","lastname":"Taylor"},{"firstname":"Katie","lastname":"Jones"},{"firstname":"Mary","lastname":"Brown"}]</answer>
-
+- Generate 2 firstnames and lastnames, for both male and female users, for the country "Germany" -> <answer>{"firstnames":["Max","Hannah"], "lastnames":["Müller","Schmidt"]}</answer>
+- Generate 4 firstnames and lastnames, for both male and female users, for the country "United Kingdom" -> <answer>{"firstnames":["George","Richard","Katie","Mary"], "lastnames":["Smith","Taylor","Jones","Brown"]}</answer>
+- Generate 3 firstnames and lastnames, for both male and female users, for the country "France" -> <answer>{"firstnames":["Lola","Mathieu","Léa"], "lastnames":["Benoît","Pierre","Lefort"]}</answer>
+- Generate 5 firstnames and lastnames, for both male and female users, for the country "Spain" -> <answer>{"firstnames":["Juan","Cristina","Javier","Julia","Isabel"], "lastnames":["Garcia","Martinez","Rodriguez","Lopez","Gomez"]}</answer>
 """
 
 prompt = """
-
-Now generate {n_user_names} typical or popular male and female names for "{country}"
-
+Generate {n_user_names} firstnames and lastnames, for both male and female users, for the country "{country}"
 """
